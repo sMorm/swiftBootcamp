bb4a34e5095bad56a52762314bdd5b24e5a3ebe8
sMorm <sereymorm@gmail.com>
2017-02-17 09:51:46 -0500

    Optimized Algorithm

--- wrappedArray.playground/Contents.swift
+++ wrappedArray.playground/Contents.swift
@@ -35,3 +35,27 @@ for item in defaultArray {
 // Finally, in Swift, we can simply 'add' arrays together, be careful of the 
 // order you are adding them by.
 wrappedArray = wrappedArray + beforeChosen
+
+
+// Because Swift is a rather high-leveled language, there are built in array
+// member functions that makes this algorithm a lot easier
+
+// For example, there are array.prefix(upTo: Int) and array.suffix(from: Int)
+// Also, there is another member function that is crucial to making this algorithm
+// work without having to specify the index, which is the array.index() member function,
+// which returns the array index value of the item.
+
+// chosenItemIndex will be assigned the index of the current item we are looping through,
+// in this case, $0, only if it is equivalent to our chosenItem.
+
+// The reason why we have a "!" at the end of this item is that the index member function
+// returns an optional Integer.
+let chosenItemIndex = defaultArray.index(where: {return $0 == chosenItem})!
+
+// array.prefix(upTo: Int) returns the array of items before our chosen value
+let prefixArray = defaultArray.prefix(upTo: chosenItemIndex)
+
+// array.suffix(from: Int) returns the remaining items in the array from the chosen value
+let suffixArray = defaultArray.suffix(from: chosenItemIndex)
+
+print("Our wrapped array with a chosen value of \(chosenItem) results to \(suffixArray + prefixArray)")
