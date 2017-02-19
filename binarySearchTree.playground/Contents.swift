5c7db9d979935ea9b4ec16ab94fa089cba0beb49
sMorm <sereymorm@gmail.com>
2017-02-19 10:58:04 -0500

    Implemented Recursive Search

--- binarySearchTree.playground/Contents.swift
+++ binarySearchTree.playground/Contents.swift
@@ -20,6 +20,18 @@ class node {
     func getValue() -> Int {
         return self.Item
     }
+    
+    func setValue(Value item: Int) {
+        self.Item = item
+    }
+    
+    func getRight() -> node? {
+        return self.rightNode
+    }
+    
+    func getLeft() -> node? {
+        return self.leftNode
+    }
 }
 
 // Left Subtree
@@ -34,6 +46,19 @@ var elevenNode = node(value: 11, left: nil, right: twelveNode)
 // Root
 var Node = node(value: 10, left: sevenNode , right: elevenNode)
 
-
+func binarySearch(toSearch searchValue: Int, Tree Node: node?) -> Bool {
+    if Node == nil {
+        return false
+    }
+    if searchValue == Node?.getValue() {
+        return true
+    } else if searchValue < Node!.getValue() {
+        return binarySearch(toSearch: searchValue, Tree: Node?.getLeft())
+    } else {
+        return binarySearch(toSearch: searchValue, Tree: Node?.getRight())
+    }
+}
 
 Node.getValue()
+
+binarySearch(toSearch: 12, Tree: Node)
