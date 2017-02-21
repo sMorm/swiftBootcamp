f6477a4b465a0920910db741b911cc16c7404793
sMorm <sereymorm@gmail.com>
2017-02-21 10:53:15 -0500

    Updated node constructor

--- binarySearchTree.playground/Contents.swift
+++ binarySearchTree.playground/Contents.swift
@@ -1,16 +1,14 @@
 //: Playground - noun: a place where people can play
 
 
-
 import UIKit
-
-var str = "Hello, playground"
 
 class node {
     var Item: Int
     var leftNode: node?
     var rightNode: node?
     
+    // node class constructor
     init(value item: Int, left leftNode: node?, right rightNode: node?) {
         self.Item = item
         self.leftNode = leftNode
@@ -33,6 +31,16 @@ class node {
         return self.leftNode
     }
 }
+
+// Illustration of our tree.
+
+/*
+            10
+           /  \
+          7    11
+         / \     \
+        4   8     12
+ */
 
 // Left Subtree
 var eightNode = node(value: 8, left: nil, right: nil)
