314385975e9bfeb7dcf6ffb605a11980f5dbd1e4
sMorm <sereymorm@gmail.com>
2017-05-06 18:53:19 -0400

    sumPair

---
+++ sumPair.playground/Contents.swift
@@ -0,0 +1,6 @@
+//: Playground - noun: a place where people can play
+
+// Given an array, find the most efficient way to find 2 index locations with two ints with the sum 8
+// Challenge 1: The array is sorted
+// Challenge 2: An unsorted array
+// Challenge 3: There exists 10+ million items in the data set
